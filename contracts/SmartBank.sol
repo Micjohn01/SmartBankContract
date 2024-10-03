9f10e65c7bc16ff062cc6165676a121b2198d413
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2024-10-03 23:58:42 +0100

    added deposit and withdraw

--- contracts/SmartBank.sol
+++ contracts/SmartBank.sol
@@ -28,5 +28,41 @@ contract SmartBank {
         return users[userAddress].exists;
     }
 
+    // Function to deposit funds to an account
+    function deposit() public payable returns (bool) {
+        require(users[msg.sender].exists, "Account does not exist");
+        User storage user = users[msg.sender];
+        user.balance += msg.value;
+        totalBankBalance += msg.value;
+        user.depositTime = block.timestamp;
+
+        return true;
+    }
+
+    // Function to get the balance of a specific user
+    function getBalance(address userAddress) public view returns (uint256) {
+        require(users[userAddress].exists, "Account does not exist");
+        User storage user = users[userAddress];
+        uint256 capital = user.balance;
+        return capital;
+    }
+
+    // Function to withdraw balance from the sender's account
+    function withdraw(uint256 amount) public returns (bool) {
+        require(users[msg.sender].exists, "Account does not exist");
+        uint256 availableBalance = getBalance(msg.sender);
+        require(availableBalance >= amount, "Insufficient balance");
+        require(totalBankBalance >= amount, "Bank does not have enough balance");
+
+        User storage user = users[msg.sender];
+        user.balance -= amount;
+        totalBankBalance -= amount;
+
+        (bool sent, ) = payable(msg.sender).call{value: amount}("");
+        require(sent, "Transaction failed");
+
+        return true;
+    }
+
 
 }
