e9b870689079bc871f8d6a79e9f57b50ace9c0e7
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2024-10-03 23:55:36 +0100

    account created

--- contracts/SmartBank.sol
+++ contracts/SmartBank.sol
@@ -13,4 +13,20 @@ contract SmartBank {
 
     mapping(address => User) private users;
 
+    function createAccount() public returns (bool) {
+        require(!users[msg.sender].exists, "Account already exists");
+        users[msg.sender] = User({
+            balance: 0,
+            depositTime: 0,
+            exists: true
+        });
+        return true;
+    }
+
+    // Function to check if an account exists
+    function accountExists(address userAddress) public view returns (bool) {
+        return users[userAddress].exists;
+    }
+
+
 }
